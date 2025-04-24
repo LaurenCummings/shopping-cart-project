5f31e6c9c0239920ed28676250eb61c9a7b41b2b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-24 01:30:59 -0400

    Started displaying product data

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,10 @@
 function ProductCard({product}) {
     return (
         <div>
-            Product Item
+            <h1>{product.title}</h1>
+            <p>{product.price.toLocaleString('en-US', {style:'currency', currency:'USD'})}</p>
+            <img src={product.image} alt={product.title} />
+            <p>{product.description}</p>
         </div>
     )
 }
