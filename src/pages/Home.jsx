2e89072ab1a51f41342650698e10f31a85318e7e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-24 01:05:03 -0400

    Added api call and loader spinner image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react';
+import { Circles } from 'react-loader-spinner';
+
 function Home() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchListOfProducts() {
+        setLoading(true)
+        const res = await fetch('https://fakestoreapi.com/products');
+        const data = await res.json();
+
+        if(data) {
+            setLoading(false);
+            setProducts(data);
+        }
+    }
+
+    useEffect(() => {
+        fetchListOfProducts();
+    }, []);
+
     return (
         <div>
-            Home
+            {loading ? (
+                <div className="min-h-screen w-full flex justify-center items-center">
+                    <Circles 
+                        height={"120"}
+                        width={"120"}
+                        color="rgb(127, 29, 29)"
+                        visible={true}
+                    />
+                </div>
+                ) : null}
         </div>
     )
 }
