5235291119ef5ad5e4e857b1d7fbca3662c840e9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-24 00:09:53 -0400

    Added routes for home and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <h1>Navbar</h1>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
   )
 }
 
