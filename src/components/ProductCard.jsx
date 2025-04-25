0afbdbcddfd09e09f31ead9bdea2e5044f01eb4b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-25 01:32:39 -0400

    Deleted ProductCard practice - added image display to ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,15 @@
 function ProductCard({product}) {
     return (
         <div>
-            <h1>{product.title}</h1>
-            <p>{product.price.toLocaleString('en-US', {style:'currency', currency:'USD'})}</p>
-            <img src={product.image} alt={product.title} />
-            <p>{product.description}</p>
+            <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+                <div className="h-[180px]">
+                    <img 
+                        src={product?.image}
+                        alt={product?.title}
+                        className="object-cover h-full w-full"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
