4d0774b682593982348374143a70e82aa3c56972
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-25 01:57:19 -0400

    Created addToCart method

--- src/store/slices/cart-slice.js
+++ src/store/slices/cart-slice.js
@@ -5,7 +5,13 @@ const initialState = []
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers : {},
-})
+    reducers : {
+        addToCart(state, action) {
+            state.push(action.payload);
+        },
+    },
+});
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
