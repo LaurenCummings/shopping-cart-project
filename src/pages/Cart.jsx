964a040d0e25357a4071d3fcf65b1c9efbf3ec6b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-26 01:50:53 -0400

    Added link and message when cart is empty

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Cart() {
     const [totalCart, setTotalCart] = useState(0);
@@ -13,7 +14,18 @@ function Cart() {
 
     return (
         <div>
-            Cart
+            {
+                cart && cart.length ? <div>
+
+                    </div> : <div className="min-h-[80vh] flex flex-col items-center justify-center">
+                        <h1 className="text-gray-800 font-bold text-xl mb-2">Your cart is empty</h1>
+                        <Link to="/">
+                            <button className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+                                SHOP NOW
+                            </button>
+                        </Link>
+                    </div>
+            }
         </div>
     )
 }
