0a225b0bdfd78567b782edf9c5ba91ea4597e716
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-27 05:24:34 -0400

    Added content to CartCard

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,7 +1,12 @@
 function CartCard({cartItem}) {
     return (
-        <div>
-            Cart Item
+        <div className="flex items-center p-5 justify-between bg-red-500 mt-2 mb-2 rounded-xl">
+            <div className="flex p-3">
+                <img src={cartItem?.image} className="h-28 rounded-lg" alt={cartItem?.title} />
+                <div className="ml-10 self-start space-y-5">
+                    <h1 className="text-xl text-white font-bold">{cartItem?.title}</h1>
+                </div>
+            </div>
         </div>
     )
 }
