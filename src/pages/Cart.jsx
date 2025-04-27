6a7cd030ee92a1c9dfdb018f5bbf1b0d5b3e66b3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-27 05:38:06 -0400

    did some style adjustments

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -14,7 +14,7 @@ function Cart() {
     console.log(cart, totalCart);
 
     return (
-        <div>
+        <div className="flex justify-center">
             {
                 cart && cart.length ? 
                 <>
@@ -26,16 +26,18 @@ function Cart() {
                     </div>
 
                 </div>
-                <div className="flex flex-col justify-center items-end p-5 space-y-5 mt-14">
-                    <h1 className="font-bold text-lg text-red-800">Your Cart Summary</h1>
-                    <p>
-                        <span className="text-gray-800 font-bold">Total Items</span>
-                        <span>: {cart.length}</span>
-                    </p>
-                    <p>
-                        <span className="text-gray-800 font-bold">Total Amount</span>
-                        <span>: {totalCart}</span>
-                    </p>
+                <div className="w-[300px]">
+                    <div className="flex flex-col justify-center items-end p-5 space-y-5 mt-14">
+                        <h1 className="font-bold text-lg text-red-800">Your Cart Summary</h1>
+                        <p>
+                            <span className="text-gray-800 font-bold">Total Items</span>
+                            <span>: {cart.length}</span>
+                        </p>
+                        <p>
+                            <span className="text-gray-800 font-bold">Total Amount</span>
+                            <span>: {totalCart}</span>
+                        </p>
+                    </div>
                 </div> 
                 </>
                 : <div className="min-h-[80vh] flex flex-col items-center justify-center">
